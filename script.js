937a5d625f827e2f3dbf003486cb71d7d1032617
rahul31agrawal <rahul31agrawal@gmail.com>
2021-09-17 22:39:12 +0530

    till-next-date

--- script.js
+++ script.js
@@ -45,11 +45,91 @@ function getAllDateFormate(date){
 
 }
 
+
+function checkPalindromeForAllDateFormats(date) {
+    var listOfPalindromes = getAllDateFormate(date);
+    
+    var flag = false;
+
+    for(var i=0; i < listOfPalindromes.length; i++){
+
+        if(isPalindrome(listOfPalindromes[i])){
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+
+function isLeapYear(year){
+    if(year % 400 === 0){
+        return true;
+    }
+    if(year % 100 === 0) {
+        return false;
+    }
+    if(year % 4 ===0){
+
+        return true;
+    }
+    return false;
+}
+
+
+
+function getNextDate(date){
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(month ===2){
+        // check for leap year
+        if(isLeapYear(year)){
+            if(day > 29){
+                day = 1;
+                month++;
+            }
+
+        }else{
+
+            if(day > 28){
+                day = 1;
+                month++;
+            }
+        }
+    }else{
+        if(day > daysInMonth[month-1]) {
+            day = 1;
+            month++;
+        }
+    }
+
+    if(month > 12) {
+        month = 1;
+        year++;
+    }
+
+    return {
+        day: date,
+        month: month,
+        year: year
+    };
+
+}
+
+
+function getNextPalindromeDate(date){
+
+}
+
+
 var date = {
 
-    day:2,
-    month:11,
+    day:28,
+    month:2,
     year: 2020
 };
 
-console.log(getAllDateFormate(date));
+console.log(getNextDate(date));
