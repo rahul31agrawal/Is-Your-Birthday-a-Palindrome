a3c16657da6ad82b32a7a17d0cc2367c729d2661
rahul31agrawal <rahul31agrawal@gmail.com>
2021-09-17 16:12:03 +0530

    added-palindrome-function

---
+++ script.js
@@ -0,0 +1,55 @@
+function reverseStr(str) {
+    var listOfChars = str.split('');
+    var reverseListOfChars = listOfChars.reverse();
+    var reversedStr = reverseListOfChars.join('');
+    return reversedStr;
+}
+
+function isPalindrome(str) {
+    var reverse = reverseStr(str);
+    return str === reverse;
+}
+
+
+
+function converDateToStr(date){
+    var dateStr = {day:'', month:'',year:''};
+    if(date.day < 10){
+        dateStr.day = '0' + date.day;
+    }else{
+        dateStr.day = date.day.toString();
+    }
+
+
+    if(date.month < 10){
+        dateStr.month = '0' + date.month;
+    }else{
+        dateStr.month = date.month.toString();
+    }
+
+    dateStr.year = date.year.toString();
+    return dateStr;
+}
+
+function getAllDateFormate(date){
+    var dateStr = converDateToStr(date);
+
+    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+    return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd];
+
+}
+
+var date = {
+
+    day:2,
+    month:11,
+    year: 2020
+};
+
+console.log(getAllDateFormate(date));
